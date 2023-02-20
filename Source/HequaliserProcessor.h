8ab75da3cecb6f842bcfbc1a02d9259209e4e99b
Cárthach Ó Nuanáin <carthach@Carthachs-MBP.station>
2023-02-20 21:52:29 +0000

    Fix includes for tests

--- Source/HequaliserProcessor.h
+++ Source/HequaliserProcessor.h
@@ -9,7 +9,8 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
-
+#include <juce_dsp/juce_dsp.h>
+#include "Analyser.h"
 
 //==============================================================================
 /**
